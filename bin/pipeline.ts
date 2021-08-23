edba830d7df4781f080db26b22ae7bb2aa16c1ea
IntegralD-503 <hunter.clark@utexas.edu>
2021-08-22 23:54:30 -0700

    Add production stage and deploy service stack

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -3,10 +3,15 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { BillingStack } from '../lib/BillingStack';
+import { ServiceStack } from '../lib/service-stack';
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', { });
+const pipelineStack = new PipelineStack(app, 'PipelineStack', { });
 new BillingStack(app, "BillingStack", {
   budgetAmount: 30,
   emailAddress:  "d503.the.integral@gmail.com"
 })
+
+const serviceStackProd = new ServiceStack(app, "SerivceStackProd");
+
+pipelineStack.addServiceStage(serviceStackProd, "Prod");
