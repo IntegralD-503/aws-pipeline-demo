820b10ec0734d981ccf5c58058e090f53c721996
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-09 17:47:23 -0500

    add build stage

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,16 +1,53 @@
 import * as cdk from 'aws-cdk-lib';
+import { SecretValue } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new Pipeline(this, 'Pipeline', {
+      pipelineName: 'Pipeline',
+      crossAccountKeys: false,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    });
+
+    const sourceOutput = new Artifact('SourceOutput');
+
+    pipeline.addStage({
+      stageName:'Source',
+      actions:[
+        new GitHubSourceAction({
+          owner: 'IntegralD-503',
+          repo: 'aws-pipeline-demo',
+          branch: 'main',
+          actionName: 'Pipeline_Source',
+          oauthToken: SecretValue.secretsManager('github-token'),
+          output: sourceOutput
+        })
+      ]
+    });
+
+    const cdkBuildOutput = new Artifact("CdkBuildOutput");
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'CDK_Build',
+          input: sourceOutput,
+          outputs: [ cdkBuildOutput ],
+          project: new PipelineProject(this, 'CdkBuildProject', {
+            environment: {
+              buildImage: LinuxBuildImage.AMAZON_LINUX_2
+            },
+            buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
+          })
+        })
+      ]
+    })
   }
 }
