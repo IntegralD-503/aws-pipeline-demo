65cebd4d59860c40f2b3a9ba5b6fbe09ef18a262
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-10 15:55:42 -0500

    Add test to build stage; Add test for app stack

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,17 +1,10 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as App from '../lib/app-stack';
+import { App } from "aws-cdk-lib"
+import { Template } from "aws-cdk-lib/assertions";
+import { AppStack } from "../lib/app-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/app-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new App.AppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('App Stack', () => {
+    const app = new App();
+    const stack = new AppStack(app, 'AppStack', {});
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+})
