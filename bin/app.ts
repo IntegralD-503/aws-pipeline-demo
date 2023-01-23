f8a20e9f4852387471c2f569bc5cf54842093f5a
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-23 16:05:55 -0500

    Add Billing update action to prod stage

--- bin/app.ts
+++ bin/app.ts
@@ -6,15 +6,17 @@ import { BillingStack } from '../lib/billing-stack';
 import { ServiceStack } from '../lib/server-stack';
 
 const app = new cdk.App();
-const pipeline = new AppStack(app, 'AppStack', {
+const pipelineStack = new AppStack(app, 'AppStack', {
   
 });
 
-new BillingStack(app, 'BillingStack', {
-  budgetAmount: 5,
+const billingStack = new BillingStack(app, 'BillingStack', {
+  budgetAmount: 30,
   emailAddress: 'hunter.clark@utexas.edu'
 });
 
 const serviceStackProd = new ServiceStack(app, 'ServiceStackProd')
 
-pipeline.addServiceStage(serviceStackProd, "Prod")
+const prodStage = pipelineStack.addServiceStage(serviceStackProd, "Prod")
+
+pipelineStack.addBillingStackToStage(billingStack, prodStage);
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,6 +1,7 @@
 import { App } from "aws-cdk-lib"
-import { Match, Template } from "aws-cdk-lib/assertions";
+import { Capture, Match, Template } from "aws-cdk-lib/assertions";
 import { AppStack } from "../lib/app-stack";
+import { BillingStack } from "../lib/billing-stack";
 import { ServiceStack } from "../lib/server-stack";
 
 test('App Stack', () => {
@@ -35,5 +36,39 @@ test('Adding Service Stage', () => {
                 Name: "Test"
             }
         ]
+    });
+
+    Template.fromStack(pipelineStack).hasResourceProperties("AWS::CodePipeline::Pipeline", {
+        Stages: Match.arrayWith( [
+            Match.objectLike({
+                Name: "Test",
+            })
+        ])
+    })
+})
+
+test("Adding Billing Stack to a Stage", () => {
+    const app = new App();
+    const serviceStack = new ServiceStack(app, "ServiceStack");
+    const pipelineStack = new AppStack(app, "AppStack");
+    const billingStack = new BillingStack(app, 'BillingStack', {
+        budgetAmount: 5,
+        emailAddress: "test@example.com"
+    });
+
+    const testStage = pipelineStack.addServiceStage(serviceStack, "Test");
+
+    pipelineStack.addBillingStackToStage(billingStack, testStage);
+
+    Template.fromStack(pipelineStack).hasResourceProperties("AWS::CodePipeline::Pipeline", {
+        Stages: Match.arrayWith([
+            Match.objectLike({
+                Actions: Match.arrayWith([
+                    Match.objectLike( {
+                        Name: "Billing_Update",
+                    })
+                ])
+            })
+        ])
     });
 })
