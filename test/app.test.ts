e888ab46242441d10e0273d0f2bd562336eb1b7b
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-23 02:43:14 -0500

    added test for addServiceStage method of AppStack

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,10 +1,39 @@
 import { App } from "aws-cdk-lib"
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 import { AppStack } from "../lib/app-stack";
+import { ServiceStack } from "../lib/server-stack";
 
 test('App Stack', () => {
     const app = new App();
     const stack = new AppStack(app, 'AppStack', {});
 
     expect(Template.fromStack(stack)).toMatchSnapshot();
+})
+
+test('Adding Service Stage', () => {
+
+    const app = new App();
+    const serviceStack = new ServiceStack(app, "ServiceStack");
+    const pipelineStack = new AppStack(app, "AppStack");
+
+    // when
+    pipelineStack.addServiceStage(serviceStack, "Test");
+
+    //then
+    Template.fromStack(pipelineStack).hasResourceProperties("AWS::CodePipeline::Pipeline", {
+        Stages: [
+            {
+                Name: "Source"
+            },
+            {
+                Name: "Build"
+            },
+            {
+                Name: "Pipeline_Update"
+            },
+            {
+                Name: "Test"
+            }
+        ]
+    });
 })
