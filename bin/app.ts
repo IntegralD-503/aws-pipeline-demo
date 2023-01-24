74071563603959a6928f70810c8481773364595f
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-24 16:47:31 -0500

    add integ test to test stage

--- bin/app.ts
+++ bin/app.ts
@@ -26,4 +26,4 @@ const testStage = pipelineStack.addServiceStage(serviceStackTest, "Test")
 const prodStage = pipelineStack.addServiceStage(serviceStackProd, "Prod")
 
 pipelineStack.addBillingStackToStage(billingStack, prodStage);
-// pipelineStack.addServiceIntegrationTest(testStage, serviceStackTest.serviceEndpointOutput.importValue);
+pipelineStack.addServiceIntegrationTest(testStage, serviceStackTest.serviceEndpointOutput.importValue);
