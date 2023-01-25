8e10e05c9b13119fc0d27ed5373234c16f5dd605
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-25 16:30:42 -0500

    add gradual deployment to service stack

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,7 +1,8 @@
 import { HttpApi } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LambdaDeploymentConfig, LambdaDeploymentGroup } from "aws-cdk-lib/aws-codedeploy";
+import { Alias, CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 interface ServiceStackProps extends StackProps{
@@ -21,13 +22,26 @@ export class ServiceStack extends Stack {
             runtime: Runtime.NODEJS_14_X,
             handler: 'src/lambda.handler',
             code: this.serviceCode,
-            functionName: `ServiceLambda${props.stageName}`
+            functionName: `ServiceLambda${props.stageName}`,
+            description:`Generated on ${new Date().toISOString()}`
+        })
+
+        const alias = new Alias(this, 'ServiceLambdaAlias', {
+            version: lambda.currentVersion,
+            aliasName: `ServiceLambdaAlias${props.stageName}`
         })
 
         const httpApi = new HttpApi(this, 'ServiceApi', {
             defaultIntegration: new HttpLambdaIntegration("LambdaIntegration", lambda),
             apiName: `MyService${props.stageName}`
         })
+
+        if (props.stageName === 'Prod') {
+            new LambdaDeploymentGroup(this, 'DeploymentGroup', {
+                alias: alias,
+                deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES
+            })
+        }
 
        this.serviceEndpointOutput = new CfnOutput(this, "ApiEndpointOutput", {
             exportName: `ServiceEndpoint${props.stageName}`,
